5a7dd790e4430fee72797e4b7400967934fefa38
dcai <heydcai@gmail.com>
2024-04-11 16:39:25 +1000

    simple rules for laptop keyboard

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,27 @@
+// prettier.config.js or .prettierrc.js
+
 /** @type {import('prettier').Options} */
 module.exports = {
-  singleQuote: true,
-  semi: false,
   trailingComma: 'all',
-  bracketSameLine: true,
-}
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  useTabs: false,
+  quoteProps: 'preserve',
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: ['*.yaml', '*.yml'],
+      options: {
+        singleQuote: false,
+        tabWidth: 2,
+      },
+    },
+  ],
+};
