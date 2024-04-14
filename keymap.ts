2cedb86cf574a17369845430d194d5e5f550f3f2
dcai <heydcai@gmail.com>
2024-04-14 23:49:31 +1000

    simlayer

--- keymap.ts
+++ keymap.ts
@@ -15,36 +15,46 @@ import {
 
 const ifAppleKeyboard = ifDevice({ vendor_id: 1452 });
 
-writeToProfile('apple_keyboard', [
-  simlayer('spacebar', 'num-layer').manipulators([
-    withCondition(ifAppleKeyboard)([
-      map('q').to('1'),
-      map('w').to('2'),
-      map('e').to('3'),
-      map('a').to('4'),
-      map('s').to('5'),
-      map('d').to('6'),
-      map('z').to('7'),
-      map('x').to('8'),
-      map('c').to('9'),
-      map('v').to('0'),
-      map('f').to('period'),
+const simlayerTimeout = 80;
+
+writeToProfile(
+  'apple_keyboard',
+  [
+    simlayer('spacebar', 'num-layer').manipulators([
+      withCondition(ifAppleKeyboard)([
+        map('q').to('1'),
+        map('w').to('2'),
+        map('e').to('3'),
+        map('a').to('4'),
+        map('s').to('5'),
+        map('d').to('6'),
+        map('z').to('7'),
+        map('x').to('8'),
+        map('c').to('9'),
+        map('v').to('0'),
+        map('f').to('period'),
+      ]),
     ]),
-  ]),
-  simlayer('s', 'nav-layer').manipulators([
-    withCondition(ifAppleKeyboard)([
-      map('h').to('left_arrow'),
-      map('j').to('down_arrow'),
-      map('k').to('up_arrow'),
-      map('l').to('right_arrow'),
+    simlayer('s', 'nav-layer').manipulators([
+      withCondition(ifAppleKeyboard)([
+        map('h').to('left_arrow'),
+        map('j').to('down_arrow'),
+        map('k').to('up_arrow'),
+        map('l').to('right_arrow'),
+      ]),
     ]),
-  ]),
-  rule('a and ; are ctrls', ifAppleKeyboard).manipulators([
-    map('a', 'optionalAny').to('left_control').toIfAlone('a'),
-    map('semicolon', 'optionalAny').to('right_control').toIfAlone('semicolon'),
-  ]),
-  rule('f and j are shift', ifAppleKeyboard).manipulators([
-    map('f', 'optionalAny').to('left_shift').toIfAlone('f'),
-    map('j', 'optionalAny').to('right_shift').toIfAlone('j'),
-  ]),
-]);
+    rule('a and ; are ctrls', ifAppleKeyboard).manipulators([
+      map('a', 'optionalAny').to('left_control').toIfAlone('a'),
+      map('semicolon', 'optionalAny')
+        .to('right_control')
+        .toIfAlone('semicolon'),
+    ]),
+    rule('f and j are shift', ifAppleKeyboard).manipulators([
+      map('f', 'optionalAny').to('left_shift').toIfAlone('f'),
+      map('j', 'optionalAny').to('right_shift').toIfAlone('j'),
+    ]),
+  ],
+  {
+    'simlayer.threshold_milliseconds': simlayerTimeout,
+  },
+);
