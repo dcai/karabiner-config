332fb9444f363c5adb7cdd9d5a3755fb7bbf7778
dcai <d@tux.im>
2024-12-31 22:33:22 +1100

    update

--- keymap.ts
+++ keymap.ts
@@ -1,50 +1,40 @@
-import {
-  NumberKeyValue,
-  duoLayer,
-  hyperLayer,
-  ifApp,
-  ifDevice,
-  ifVar,
-  layer,
-  map,
-  mapDoubleTap,
-  rule,
-  simlayer,
-  toApp,
-  withCondition,
-  withMapper,
-  writeToProfile,
-} from 'karabiner.ts';
+import * as K from 'karabiner.ts';
 
-const ifAppleKeyboard = ifDevice({ vendor_id: 1452 });
+const ifAppleKeyboard = K.ifDevice({ vendor_id: 1452 });
 
 const rules = [
-  layer('/', 'num-layer').manipulators([
-    withCondition(ifAppleKeyboard)([
-      map('q').to('1'),
-      map('w').to('2'),
-      map('e').to('3'),
-      map('a').to('4'),
-      map('s').to('5'),
-      map('d').to('6'),
-      map('z').to('7'),
-      map('x').to('8'),
-      map('c').to('9'),
-      map('v').to('0'),
-      map('f').to('period'),
+  K.layer('/', 'num-layer').manipulators([
+    K.withCondition(ifAppleKeyboard)([
+      K.map('q').to('1'),
+      K.map('w').to('2'),
+      K.map('e').to('3'),
+      K.map('a').to('4'),
+      K.map('s').to('5'),
+      K.map('d').to('6'),
+      K.map('z').to('7'),
+      K.map('x').to('8'),
+      K.map('c').to('9'),
+      K.map('v').to('0'),
+      K.map('f').to('period'),
     ]),
   ]),
-  layer('caps_lock', 'nav-layer').manipulators([
-    withCondition(ifAppleKeyboard)([
-      map('h').to('left_arrow'),
-      map('j').to('down_arrow'),
-      map('k').to('up_arrow'),
-      map('l').to('right_arrow'),
+  K.layer('caps_lock', 'nav-layer').manipulators([
+    K.withCondition(ifAppleKeyboard)([
+      K.map('h').to('left_arrow'),
+      K.map('j').to('down_arrow'),
+      K.map('k').to('up_arrow'),
+      K.map('l').to('right_arrow'),
     ]),
   ]),
-  rule('a and ; are ctrls', ifAppleKeyboard).manipulators([
-    // map('a', 'optionalAny').to('left_control', undefined, {}).toIfAlone('a'),
-    map('semicolon', 'optionalAny').to('right_control').toIfAlone('semicolon'),
+  K.rule('; is ctrl', ifAppleKeyboard).manipulators([
+    K.map('semicolon', 'optionalAny')
+      .to('right_control')
+      .toIfAlone('semicolon'),
+  ]),
+  K.rule('enter key is shift + ctrl', ifAppleKeyboard).manipulators([
+    K.map('return_or_enter', 'optionalAny')
+      .to('left_shift', 'left_control')
+      .toIfAlone('return_or_enter'),
   ]),
   // rule('capslock is ctrl and esc', ifAppleKeyboard).manipulators([
   //   map('caps_lock', 'optionalAny').to('left_control').toIfAlone('escape'),
@@ -55,7 +45,7 @@ const rules = [
   // ]),
 ];
 
-writeToProfile('dcai', rules, {
+K.writeToProfile('dcai', rules, {
   'simlayer.threshold_milliseconds': 80,
   'basic.to_if_alone_timeout_milliseconds': 500,
   'basic.to_if_held_down_threshold_milliseconds': 500,
